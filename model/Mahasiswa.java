098a9089d0a96f1298bb8a2a82a8c8899a8c82af
WiraJB <wirajulian88@gmail.com>
2020-03-23 13:38:23 +0800

    Memanggil method dari banyak Class di package lain

---
+++ model/Mahasiswa.java
@@ -0,0 +1,11 @@
+package model;
+
+public class Mahasiswa{
+ public static void tampilanInfo(){
+  System.out.println();
+  System.out.println("== Data Mahasiswa ====================");
+  System.out.println("NPM  : 18630329");
+  System.out.println("Nama : Wira Julian Bayangkara");
+  System.out.println("======================================");
+ }
+}
